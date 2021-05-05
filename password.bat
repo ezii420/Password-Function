23500d08f43a656ee3e275452b35860d9c238762
ezii420 <83398375+ezii420@users.noreply.github.com>
2021-05-05 09:02:21 +0300

    Create password.bat

---
+++ password.bat
@@ -0,0 +1,6 @@
+@echo off
+set /p user= Enter your username: 
+set /p pass= Enter your password: 
+if %user% /* Your username */ if %pass% /* Your password  */ GoTo main
+:main
+//Over here you will paste/enter your code
